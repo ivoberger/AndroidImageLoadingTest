f89c22ad14f282cdf6d08a7790519528519e98ed
Maja Noell <m.noell@outlook.de>
2019-08-20 22:44:15 +0200

    additional renaming

--- app/src/main/java/com/ivoberger/imageloadingtest/ImagePyramidAdapter.kt
+++ app/src/main/java/com/ivoberger/imageloadingtest/ImagePyramidAdapter.kt
@@ -5,12 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 
 class ImagePyramidAdapter(itemCount: Int) :
-    BaseAdapter<ImagePyramidAdapter.MultiImageViewHolder>(itemCount) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MultiImageViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.viewholder_multiimage, parent, false)
+    BaseAdapter<ImagePyramidAdapter.ViewHolder>(itemCount) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.viewholder_image_pyramid,
+            parent,
+            false
+        )
     )
 
-    class MultiImageViewHolder(itemView: View) : BaseViewHolder(itemView) {
+    class ViewHolder(itemView: View) : BaseViewHolder(itemView) {
         override fun onBind(position: Int) {
             val imgUrl = "https://picsum.photos/id/${position * 2}/500"
             // TODO load image into image views
