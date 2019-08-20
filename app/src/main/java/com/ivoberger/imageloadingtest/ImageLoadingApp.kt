63104b2a35435d3ab061c00b4163ac77dcd28ee7
Maja Noell <m.noell@outlook.de>
2019-08-20 23:11:57 +0200

    added comment about 'unused' class

--- app/src/main/java/com/ivoberger/imageloadingtest/ImageLoadingApp.kt
+++ app/src/main/java/com/ivoberger/imageloadingtest/ImageLoadingApp.kt
@@ -3,6 +3,7 @@ package com.ivoberger.imageloadingtest
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
 
+// Needs to be registered in manifest for image loading library
 class ImageLoadingApp : Application() {
 
     override fun onCreate() {
