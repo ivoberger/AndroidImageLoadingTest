25b08796fd62e0692ed582fadcad4d42ceebbbd8
Ivo Berger <ivo@ivoberger.com>
2019-08-20 22:59:10 +0200

    refactor

--- app/src/main/java/com/ivoberger/imageloadingtest/ImagePyramidAdapter.kt
+++ app/src/main/java/com/ivoberger/imageloadingtest/ImagePyramidAdapter.kt
@@ -19,10 +19,10 @@ class ImagePyramidAdapter(itemCount: Int) :
         override fun onBind(position: Int) {
             val imgUrl = "https://picsum.photos/id/${position * 2}/500"
             val imageViews = listOf<ImageView>(
-                itemView.findViewById(R.id.imageView_multiImageViewHolder_1),
-                itemView.findViewById(R.id.imageView_multiImageViewHolder_2),
-                itemView.findViewById(R.id.imageView_multiImageViewHolder_3),
-                itemView.findViewById(R.id.imageView_multiImageViewHolder_4)
+                itemView.findViewById(R.id.imageView_imagePyramidViewHolder_1),
+                itemView.findViewById(R.id.imageView_imagePyramidViewHolder_2),
+                itemView.findViewById(R.id.imageView_imagePyramidViewHolder_3),
+                itemView.findViewById(R.id.imageView_imagePyramidViewHolder_4)
             )
             imageViews.forEach { imageView ->
                 // TODO load image into image views
